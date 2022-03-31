e2a6a333c76458eb521838cb51876aea6a9a50e5
Polina Gurtovaya <zloymult@gmail.com>
2022-04-01 01:10:00 +0300

    Add server styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app'
 import { Layout } from '../components/Layout'
 import { ThemeProvider } from 'next-themes'
 import { Analytics } from '../components/Analytics'
+import { getCssText } from '@styles'
 
 function NextAPP({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="dark">
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <Analytics />
       <Layout>
         <Component {...pageProps} />
