866f4346229345b6dbdd39217385e724017af094
Polina Gurtovaya <zloymult@gmail.com>
2022-10-30 17:55:19 +0400

    Cleaning :) (#5)

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -4,13 +4,6 @@ import { Post } from '../types/blog'
 function generateSiteMap(posts: Post[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
      ${posts
        .map(({ slug }) => {
          return `
@@ -27,7 +20,6 @@ function generateSiteMap(posts: Post[]) {
 function SiteMap() {}
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(
     // @ts-ignore
     __POSTS__.filter(p => !skipUnpublished || p.frontmatter.published)
