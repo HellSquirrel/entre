29171f80a2e3fdfe927469989b2e25193e00edef
Polina Gurtovaya <zloymult@gmail.com>
2023-02-26 21:32:54 +0400

    Add language switcher

---
+++ components/LangSwitcher.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import { useRouter } from 'next/router'
+import { styled } from '@styles'
+import { Link } from './Link'
+
+type Props = {
+  className?: string
+}
+
+const StyledLink = styled('button', {
+  border: 'none',
+  background: 'none',
+  fontSize: '$main',
+  outline: 'none',
+  transition: 'opacity 300ms',
+  color: '$white !important',
+  fontWeight: 'normal',
+
+  '&:hover': {
+    opacity: 0.8,
+  },
+})
+
+export const LangSwitcher: FC<Props> = ({ className }) => {
+  const router = useRouter()
+  const { locale, pathname } = router
+  const nextLocale = locale === 'en' ? 'ru' : 'en'
+
+  return (
+    <StyledLink
+      onClick={() => router.push(pathname, pathname, { locale: nextLocale })}
+      className={className}>
+      {nextLocale}
+    </StyledLink>
+  )
+}
