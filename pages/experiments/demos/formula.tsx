d9378f15eef917ff8852fffdd89c920daa64011c
Polina Gurtovaya <zloymult@gmail.com>
2024-03-28 23:17:26 +0000

    Remove experiments

--- pages/experiments/demos/formula.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import { Formula } from '../../../components/Formula'
-
-const FormulaDemo: FC = () => (
-  <Formula content="\forall x \in X, \quad \exists y \leq \epsilon" />
-)
-
-export default FormulaDemo
--- pages/experiments/demos/pkg/entre_graph_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function fractal_square_points(a: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
