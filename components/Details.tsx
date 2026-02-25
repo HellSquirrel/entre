a7fcfb1aa2f61952fea3817c6266716f62a32610
Polina Gurtovaya <zloymult@gmail.com>
2026-02-25 23:01:43 +0000

    Add cats and details

--- components/Details.tsx
+++ components/Details.tsx
@@ -4,14 +4,29 @@ import { styled } from '@styles'
 type Props = {
   title: string
   children: React.ReactNode
+  withBackground?: boolean
 }
 
 const StyledDetails = styled('details', {
   padding: '$3 0',
+
+  variants: {
+    withBackground: {
+      true: {
+        backgroundColor: '$violet4',
+        padding: '$3',
+        borderRadius: '6px',
+
+        [`html[data-theme='dark'] &`]: {
+          backgroundColor: 'hsl(255, 30%, 20%)',
+        },
+      },
+    },
+  },
 })
 
-export const Details = ({ children, title } : Props) => (
-  <StyledDetails>
+export const Details = ({ children, title, withBackground }: Props) => (
+  <StyledDetails withBackground={withBackground}>
     <summary>{title}</summary>
     {children}
   </StyledDetails>
